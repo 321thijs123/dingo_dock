688d50e2a31b268fe6e4d109d2e295ba04e7d25c
321thijs123 <thijs.hof@home.nl>
2021-04-01 13:57:07 +0200

    First commit

---
+++ src/cloudfilter.cpp
@@ -0,0 +1,73 @@
+#include "ros/ros.h"
+#include <vector>
+#include "sensor_msgs/point_cloud_conversion.h"
+
+const float max_height = 0.05;
+const float min_height = -0.05;
+
+sensor_msgs::PointCloud filtered_cloud;
+
+void cloudCallback(const sensor_msgs::PointCloud2);
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "cloudfilter");
+
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("velodyne_points", 100, cloudCallback);
+	ros::Publisher cloud_pub = n.advertise<sensor_msgs::PointCloud>("filtered_cloud", 100);
+	
+	ros::Rate loop_rate(10);
+	
+	while (ros::ok()){
+		cloud_pub.publish(filtered_cloud);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	return 0;
+}
+
+void cloudCallback(const sensor_msgs::PointCloud2 msg)
+{
+	double startTime = ros::Time::now().toSec();
+
+	sensor_msgs::PointCloud cloud;
+
+	convertPointCloud2ToPointCloud(msg, cloud);
+	
+	std::vector<geometry_msgs::Point32> layer_points = {};
+	
+	for (int i = 0; i < cloud.points.size(); i++){
+		if (cloud.points[i].z < max_height && cloud.points[i].z > min_height){
+			layer_points.push_back(cloud.points[i]);
+		}
+	}
+
+	std::vector<geometry_msgs::Point32> object_points = {};
+
+	for (int i = 0; i < layer_points.size(); i++){
+		bool keep = true;
+
+		for (int j = 0; j < layer_points.size(); j++){
+			float distance =	(layer_points[i].x-layer_points[j].x)*
+								(layer_points[i].x-layer_points[j].x)+
+								(layer_points[i].y-layer_points[j].y)*
+								(layer_points[i].y-layer_points[j].y);
+
+			if (distance > 0.15*0.15 && distance < 0.30*0.30){
+				keep = false;
+				break;
+			}
+		}
+
+		if (keep) object_points.push_back(layer_points[i]);
+	}
+	
+	filtered_cloud.header = cloud.header;
+	filtered_cloud.points = object_points;
+
+	ROS_INFO("%lu points in %.2f seconds", object_points.size(), ros::Time::now().toSec()-startTime);
+}
